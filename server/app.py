5e4c9141c06f537b33622265bf757f0db65802e6
tommy <tommydneill@gmail.com>
2022-02-06 17:48:28 -0800

    Added API routes related to making posts

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import datetime
 
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
@@ -20,6 +21,9 @@ app = Flask(__name__)
 
 #look mom, here's some important backend!
 
+posts = db.collection('posts')
+comments = db.collection('comments')
+
 x = 0
 @app.route('/api/inc')
 def hello_world():
@@ -35,6 +39,58 @@ def add():
 @app.route('/api/read',methods=['GET'])
 def read():
     return ",\n".join(i.to_dict()["body"] for i in db.collection('posts').stream())
+
+def commend(user):
+    #TODO: store that this user was commended
+    print("Would commend",user)
+
+@app.route('/api/makePost',methods=['POST'])
+def makePost():
+    json = {"author":request.json['id'], 'points':0, 'title': request.json['title'], 'body': request.json['body'], 'date':  datetime.now().strftime("%m/%d/%Y"), 'answers': []}
+    posts.add(json)
+    return "OK",200
+
+
+@app.route('/api/makeComment',methods=['POST'])
+def makeComment():
+    parent = request.json['parent']
+    pdoc = posts.document(parent).get().to_dict()
+    newComment = {'author': request.json['author'], 'body': request.json['body'], 'parent': request.json['parent'],"points":0}
+    pdoc['answers'].append(newComment)
+    posts.document(parent).set(pdoc)
+    return "OK",200
+
+def makeDict(reference):
+    r2 = reference.to_dict()
+    r2['id'] = reference.id
+    return r2
+
+@app.route('/api/searchPosts',methods=['GET'])
+def searchPosts():
+    if 'answered' in request.args:
+        if request.args['answered'] == 'false':
+            q = posts.where('answers', '==', []).order_by('date')
+        else:
+            q = posts.order_by('answers').where('answers', '!=', []).order_by('date')
+    else:
+        q = posts.order_by('date')
+    resCount = 10
+    q = q.limit(resCount).offset(int(request.args['page'])*resCount)
+    results = [makeDict(i) for i in q.stream()]
+    return {'result': results}
+
+@app.route('/api/like',methods=['POST'])
+def like():
+    parent = request.json['id']
+    target = int(request.json['index'])
+    pdoc = posts.document(parent).get().to_dict()
+    tochange = pdoc
+    if target > 0:
+        tochange = pdoc['answers'][target-1]
+    tochange['points'] += 1
+    commend(tochange['author'])
+    posts.document(parent).set(pdoc)
+    return "OK",200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
